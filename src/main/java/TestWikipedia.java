8c2bb627ec33ed72e951858649432f34d534e30f
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-10-11 19:00:49 +0400

    add new method search

--- src/main/java/TestWikipedia.java
+++ src/main/java/TestWikipedia.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class TestWikipedia {
     private WebDriver chromeDriver;
@@ -21,13 +24,23 @@ public class TestWikipedia {
 
     @Test
     public void testChooseRussianLanguage() {
-
         WebElement ruLang = chromeDriver.findElement(By.xpath("//a[@id='js-link-box-ru']"));
         ruLang.click();
         chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         String titlePage = chromeDriver.getTitle();
         Assert.assertEquals(titlePage, "Википедия — свободная энциклопедия");
     }
+
+    @Parameters({"search"})
+    @Test(description = "check the search, if a result is empty, the test is not passed")
+    public void testSearch(String search){
+        WebElement searchForm = chromeDriver.findElement(By.id("searchInput"));
+        searchForm.sendKeys(search, Keys.ENTER);
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        List<WebElement> listResults = chromeDriver.findElements(By.xpath("//ul[@class='mw-search-results']/li"));
+        Assert.assertTrue(listResults.size()>0);
+    }
+
 
 
 }
