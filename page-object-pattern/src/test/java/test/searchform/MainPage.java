cc77f3be3539a84981c64a7a36ee605a12fc066b
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-10-19 16:43:04 +0400

    created new class MainPage

---
+++ page-object-pattern/src/test/java/test/searchform/MainPage.java
@@ -0,0 +1,35 @@
+package test.searchform;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class MainPage {
+    public WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(id = "searchInput")
+    private WebElement searchField;
+
+    @FindBy(xpath = "//ul[@class='mw-search-results']/li")
+    private List<WebElement> searchResult;
+
+    public void inputAndFindWord(String word){
+        searchField.sendKeys(word, Keys.ENTER);
+    }
+
+    public int getResultOfSearch(){
+        return searchResult.size();
+    }
+
+
+
+}
