7f29b2a3f21c3fa338cfdc51a6507b954412c927
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-10-19 16:41:55 +0400

    created new class Language

---
+++ page-object-pattern/src/test/java/test/language/LanguageTest.java
@@ -0,0 +1,75 @@
+package test.language;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import test.ConfProperties;
+
+import java.time.Duration;
+
+public class LanguageTest {
+    public static WebDriver driver;
+    public static LanguagePage languagePage;
+    private static final String en = "en";
+    private static final String de = "de";
+    private static final String fr = "fr";
+    private static final String ru = "ru";
+    private static final String es = "es";
+
+
+
+    @BeforeClass(alwaysRun = true)
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        driver = new ChromeDriver();
+        languagePage = new LanguagePage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(ConfProperties.getProperty("languagepage"));
+        languagePage.clickBtnReadWiki();
+    }
+
+
+    @AfterMethod
+    public void backOnLanguagePage(){
+        driver.navigate().back();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void englishTest() {
+        languagePage.clickEnglish();
+        Assert.assertTrue(driver.getCurrentUrl().contains(en));
+    }
+
+    @Test
+    public void spanishTest() {
+        languagePage.clickSpanish();
+        Assert.assertTrue(driver.getCurrentUrl().contains(es));
+    }
+
+    @Test
+    public void russianTest() {
+        languagePage.clickRussian();
+        Assert.assertTrue(driver.getCurrentUrl().contains(ru));
+    }
+
+    @Test
+    public void frenchTest() {
+        languagePage.clickFrench();
+        Assert.assertTrue(driver.getCurrentUrl().contains(fr));
+    }
+
+    @Test
+    public void germanTest() {
+        languagePage.clickGerman();
+        Assert.assertTrue(driver.getCurrentUrl().contains(de));
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void close(){
+        driver.quit();
+    }
+
+}
